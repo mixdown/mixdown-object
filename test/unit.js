ac9b6004bd436a7627dbdbe88455f43a596de29e
Jason M Harrison <jason.m.harrison@gmail.com>
2014-07-16 16:33:29 -0500

    Update unit.js
    Removed Broadway and adjusted unit tests to run successfully.

--- test/unit.js
+++ test/unit.js
@@ -1,26 +1,21 @@
 var assert = require('assert');
 var MixdownObject = require('../index.js');
-var broadway = require('broadway');
+var App = require('mixdown-app').App;
 
 suite('Mixdown Object', function() {
 
-  var app = {
-    plugins: new broadway.App()
-  };
-  var namespace = (Math.random() * 1000).toString();
-  var obj = {
-    foo: "bar"
-  };
+  var app = new App();
+  var namespace = "foo";
+  var obj = new MixdownObject(namespace);
 
   setup(function(done) {
-    app.plugins.use(new MixdownObject(namespace), obj);
+    app.use(obj, namespace);
     done();
   });
 
   test('Attach', function(done) {
-
-    assert.ok(app.plugins[namespace], 'Attach should have happened at correct namespace');
-    assert.deepEqual(app.plugins[namespace], obj, 'Attached object should be correct.');
+    assert.ok(app[namespace], 'Attach should have happened at correct namespace');
+    assert.deepEqual(app[namespace], obj, 'Attached object should be correct.');
     done();
 
   });
